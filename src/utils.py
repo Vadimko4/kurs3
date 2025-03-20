b3fc3daecc651fda55e9ba649025e81db72bbae8
vadimko4 <vad.informat@gmail.com>
2025-03-20 15:52:52 +0400

    add dir data and file add utils get_greeting

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import datetime
+
+
+def get_greeting() -> str:
+    """
+    Возвращает адекватное приветствие, в зависимости от времени вызова:
+    (4:01 - 11:00) Доброе утро!/(11:01 - 17:00) Добрый день!/
+    (17:01 - 23:00) Добрый вечер!/(23:01 - 5:00) Доброй ночи!
+    """
+    current_date_time = datetime.datetime.now()
+    hh = current_date_time.hour
+    mm = current_date_time.minute
+    ss = current_date_time.second
+
+    if hh in range(5, 11) or (hh == 4 and (mm > 0 or ss > 0)) or (hh == 11 and (mm == 0 or ss == 0)):
+        greeting = 'Доброе утро!'
+    elif hh in range(12, 17) or (hh == 11 and (mm > 0 or ss > 0)) or (hh == 17 and (mm == 0 or ss == 0)):
+        greeting = 'Добрый день!'
+    elif hh in range(18, 23) or (hh == 17 and (mm > 0 or ss > 0)) or (hh == 23 and (mm == 0 or ss == 0)):
+        greeting = 'Добрый вечер!'
+    else:
+        greeting = 'Доброй ночи!'
+    return greeting
