3630b59a6cb7a1a40d8371a0ac6e241ab4df9baf
vadimko4 <vad.informat@gmail.com>
2025-03-23 02:32:32 +0400

    update views

--- src/views.py
+++ src/views.py
@@ -1,12 +1,10 @@
-from datetime import datetime
-
 import pandas as pd
 
 from utils import (PATH_TO_OPERATIONS_XLSX_FILE, get_operations_from_xlsx, filter_by_state, filter_by_date,
                    filter_by_card, get_card_total_rub_spent, get_card_cashback_rub)
 
 
-def get_cards_information(operations_list: list[dict], target_date: str) -> list[dict]:
+def get_cards_information(operations_list: list[dict], start_date: str, end_date) -> list[dict]:
     """
     Принимает список словарей с данными о банковских операциях, и целевую дату - с начала месяца по которую
     нужно сформировать отчёт;
@@ -14,14 +12,6 @@ def get_cards_information(operations_list: list[dict], target_date: str) -> list
     во входном списке операций: последние 4 цифры номера карты, общая сумма расходов,
     кешбэк (1 рубль на каждые 100 рублей)
     """
-    # отфильтровываем только операции со статусом ОК
-    operations_list = filter_by_state(operations_list)
-
-    # отфильтровываем операции с нужными датами
-    start_date = f"01{target_date[2:]} 00:00:00"
-    end_date = f"{target_date} 23:59:59"
-    operations_list = filter_by_date(operations_list, start_date, end_date)
-
     # получаем номера карт, убиваем nan
     card_numbers_list = list(set(i.get("Номер карты") for i in operations_list if pd.notna(i.get("Номер карты"))))
 
@@ -39,7 +29,27 @@ def get_cards_information(operations_list: list[dict], target_date: str) -> list
     return cards_information_list
 
 
+def get_top_five_transactions(operations_list: list[dict]) -> list[dict]:
+    pass
+
+
+def get_currency_rates(date: str) -> list[dict]:
+    pass
+
+
+def get_stock_prices(date: str) -> list[dict]:
+    pass
+
+
 if __name__ == '__main__':
     request_date = input("Введите интересующую Вас дату: ")
+    start_date_operation = f"01{request_date[2:]} 00:00:00"
+    end_date_operation = f"{request_date} 23:59:59"
     operations = get_operations_from_xlsx(PATH_TO_OPERATIONS_XLSX_FILE)
-    print(get_cards_information(operations, request_date))
+
+    # отфильтровываем только операции со статусом ОК
+    operations = filter_by_state(operations)
+
+    # отфильтровываем операции с нужными датами
+    operations = filter_by_date(operations, start_date_operation, end_date_operation)
+    print(get_cards_information(operations, start_date_operation, end_date_operation))
