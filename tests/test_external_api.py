12b2703197fddfe11eda2acabbd3e1f83edb3e92
vadimko4 <vad.informat@gmail.com>
2025-03-24 21:05:18 +0400

    update test_external_api, add test_get_stock_rub_price

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 import pytest
 
-from src.external_api import get_rub_transaction_amount, get_currency_too_rub_rate
+from src.external_api import get_rub_transaction_amount, get_currency_too_rub_rate, get_stock_rub_price, stocks_api_key
 
 
 @patch('requests.get')
@@ -25,3 +25,27 @@ def test_get_currency_too_rub_rate(mock_get):
     mock_get.return_value.json.return_value = {'info': {"rate": 1000.00}}
     assert get_currency_too_rub_rate('USD') == 1000.00
     mock_get.assert_called_once()
+
+
+#  как тестировать функцию, которая содержит сначала обращение к api,
+#  а потом вызов другой функции, которая тоже внутри себя содержит ещё одно
+#  обращение  - к другой api
+@patch('requests.get')
+@patch('src.external_api.get_rub_transaction_amount')
+def test_get_stock_rub_price(mock_get_rub_transaction_amount, mock_get_stock_price):
+    mock_get_stock_price.return_value.json.return_value = {'data': [{"open": 100.00}]}
+    mock_get_rub_transaction_amount.return_value = 10000.00
+    # Вызываем тестируемую функцию
+    rub_amount = get_stock_rub_price('TSLA')
+
+    # Проверяем, что функция возвращает ожидаемое значение
+    assert rub_amount == 10000.00
+
+    # Проверяем, что оба API были вызваны
+    expected_url = f"https://api.marketstack.com/v1/eod?access_key={stocks_api_key}"
+    expected_params = {"symbols": 'TSLA'}
+    mock_get_stock_price.assert_called_once_with(expected_url, params=expected_params)
+    mock_get_rub_transaction_amount.assert_called_once_with({
+        "Сумма операции": 100,
+        "Валюта операции": "USD"
+    })
