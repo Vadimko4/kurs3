ecb3c618ca93689c8a2cdbc976a66d4d2c746d3a
vadimko4 <vad.informat@gmail.com>
2025-03-25 11:12:59 +0400

    add logger, decorators

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+def log(filename: str = '') -> Any:
+    """
+    Декоратор логирует результаты выполнения функции, или возникшие ошибки.
+    Принимает filename, который определяет,куда будут записываться логи (в файл или в консоль):
+    если filename задан, логи записываются в указанный файл; нет - логи выводятся в консоль.
+    Логирование включает: имя функции и  результат выполнения при успешной операции, либо
+    имя функции, тип возникшей ошибки и входные параметры, если выполнение функции привело к ошибке;
+    """
+    def decorator(func):
+        def wraper(*args, **kwargs):
+            if filename:
+                with open(filename, "a", encoding='utf-8') as file:
+                    try:
+                        result = func(*args, **kwargs)
+                        file.write(f"{func.__name__} ok\n")
+                        return result
+                    except Exception as e:
+                        file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
+                        raise Exception(e)
+            else:
+                try:
+                    result = func(*args, **kwargs)
+                    print(f"{func.__name__} ok")
+                    return result
+                except Exception as e:
+                    print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                    raise Exception(e)
+        return wraper
+    return decorator
---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+PATH_TO_LOGS_DIR = os.path.join(os.path.dirname(__file__)[:-4], "logs")
+APPLICATION_LOG_FILE_NAME = os.path.join(PATH_TO_LOGS_DIR, "application.log")
+
+
+# Основная конфигурация logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename=APPLICATION_LOG_FILE_NAME,  # Запись логов в файл
+                    encoding='utf-8',
+                    filemode='w')  # Перезапись файла при каждом запуске
+
+
+# Создаем логеры для различных компонентов программы
+utils_logger = logging.getLogger('utils')
+masks_logger = logging.getLogger('external_api')
