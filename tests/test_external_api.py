e4330c68bffd6bd563a9b6a51d687b3fb51c6bca
vadimko4 <vad.informat@gmail.com>
2025-03-31 22:05:26 +0400

    update test_external_api

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+import pandas as pd
 
 import pytest
 
@@ -6,18 +7,10 @@ from src.external_api import get_currency_too_rub_rate, get_rub_transaction_amou
 
 
 @patch('requests.get')
-def test_get_rub_transaction_amount(mock_get, test_operation_list):
+def test_get_rub_transaction_amount(mock_get):
     mock_get.return_value.json.return_value = {'result': 10000.00}
-    assert get_rub_transaction_amount(test_operation_list[1]) == -10000.00
+    assert get_rub_transaction_amount(-1.00, 'TRY') == -10000.00
     mock_get.assert_called_once()
-
-    assert get_rub_transaction_amount(test_operation_list[0]) == -250.00
-
-
-@pytest.mark.parametrize('wrong_transaction', [{}, {"Сумма операции": -250.0}, {"Валюта операции": "USD"}])
-def test_get_rub_wrong_transaction_amount(wrong_transaction):
-    with pytest.raises(ValueError):
-        get_rub_transaction_amount(wrong_transaction)
 
 
 @patch('requests.get')
